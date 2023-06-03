cdaa6c4a5c0ad9b8d9797bb94a47aaf5c39fc9bd
Sahan Weerakkody <it22143068@my.sliit.lk>
2023-06-04 02:27:47 +0530

    make popcollection responsive

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+// Get the previous and next buttons
+const previousButton = document.querySelector('.pre_button');
+const nextButton = document.querySelector('.post_button');
 
+// Get the product container and the width of a single product item
+const productContainer = document.querySelector('.product-container');
+const productItemWidth = document.querySelector('.product-item').offsetWidth;
+
+// Add event listeners to the previous and next buttons
+previousButton.addEventListener('click', scrollToPrevious);
+nextButton.addEventListener('click', scrollToNext);
+
+// Function to scroll to the previous item
+function scrollToPrevious() {
+  productContainer.scrollBy({
+    left: -productItemWidth,
+    behavior: 'smooth'
+  });
+}
+
+// Function to scroll to the next item
+function scrollToNext() {
+  productContainer.scrollBy({
+    left: productItemWidth,
+    behavior: 'smooth'
+  });
+}
